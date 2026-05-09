8c54a1c3dd8546854d5f18a235a2cdbd3e80ca4c
Macbook <macbook@Adityas-MacBook-Air-4.local>
2026-05-09 13:04:48 +0530

    added google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles.css';
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-Y5WV73XLV6");
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
