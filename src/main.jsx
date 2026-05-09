f593fda9ceec9d7f37485ee391d7767ee842aa33
Macbook <macbook@Adityas-MacBook-Air-4.local>
2026-05-09 13:10:28 +0530

    fixed analytics render issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from "react-ga4";
+
 import App from './App.jsx';
 import './styles.css';
 
-import ReactGA from "react-ga4";
-
 ReactGA.initialize("G-Y5WV73XLV6");
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
